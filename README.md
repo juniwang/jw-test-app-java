7383a8119507db6fc91e98c81254c7169d0d4321
Junbo Wang <juniwang@microsoft.com>
2017-07-10 14:32:25 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jw-test-app-java
-Java projects to do all kinds of testing
+Java projects to do all kinds of testing.
+
--- src/main/java/com/juniwang/App.java
+++ src/main/java/com/juniwang/App.java
@@ -1,13 +1,10 @@
 package com.juniwang;
 
-import com.juniwang.docker.JDocker;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        JDocker docker = new JDocker();
-        docker.ping();
+        System.getProperties().list(System.out);
     }
 }
